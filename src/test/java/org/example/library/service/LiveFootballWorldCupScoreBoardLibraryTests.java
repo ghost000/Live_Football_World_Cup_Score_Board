48cda3f7530b44745c8bff211ef357b4edffd359
neo-win <radixblade100@gmail.com>
2024-09-09 17:49:28 +0200

    Hard to create tests for parallelizing operations on the scoreBoard Library.

--- src/test/java/org/example/library/service/LiveFootballWorldCupScoreBoardLibraryTests.java
+++ src/test/java/org/example/library/service/LiveFootballWorldCupScoreBoardLibraryTests.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LiveFootballWorldCupScoreBoardLibraryTests {
     private LiveFootballWorldCupScoreBoardLibrary scoreBoardLibrary;
@@ -39,9 +44,7 @@ public class LiveFootballWorldCupScoreBoardLibraryTests {
 
     @Test
     void updateScoreShouldFailForNonexistentMatch() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            scoreBoardLibrary.updateScore(1234567890, 0, 1);
-        });
+        assertThrows(IllegalArgumentException.class, () -> scoreBoardLibrary.updateScore(1234567890, 0, 1));
     }
 
     @Test
@@ -59,9 +62,7 @@ public class LiveFootballWorldCupScoreBoardLibraryTests {
     @Test
     void updateScoreShouldHandleNegativeScoresGracefully() {
         final int id = scoreBoardLibrary.addNewGame("home team", "away team" );
-        assertThrows(IllegalArgumentException.class, () -> {
-            scoreBoardLibrary.updateScore(id, -10, -1);
-        });
+        assertThrows(IllegalArgumentException.class, () -> scoreBoardLibrary.updateScore(id, -10, -1));
     }
 
     @Test
@@ -101,9 +102,9 @@ public class LiveFootballWorldCupScoreBoardLibraryTests {
 
     @Test
     void finishGameShouldRemoveMatchFromScoreboard() {
-        final int id_1 = scoreBoardLibrary.addNewGame("home team 1", "away team 1");
-        final int id_2 = scoreBoardLibrary.addNewGame("home team 2", "away team 2");
-        final int id_3 = scoreBoardLibrary.addNewGame("home team 3", "away team 3");
+        final int id_1 = scoreBoardLibrary.addNewGame("home team 1", "away team 1" );
+        final int id_2 = scoreBoardLibrary.addNewGame("home team 2", "away team 2" );
+        final int id_3 = scoreBoardLibrary.addNewGame("home team 3", "away team 3" );
 
         scoreBoardLibrary.updateScore(id_1, 1, 1);
         scoreBoardLibrary.updateScore(id_2, 2, 2);
@@ -118,6 +119,50 @@ public class LiveFootballWorldCupScoreBoardLibraryTests {
         List<String[]> expectedTeams = List.of(
                 new String[]{"home team 3", "away team 3"},
                 new String[]{"home team 1", "away team 1"}
+        );
+
+        for (int i = 0; i < expectedTeams.size(); i++) {
+            assertEquals(expectedTeams.get(i)[0], summary.get(i).getHomeTeamName());
+            assertEquals(expectedTeams.get(i)[1], summary.get(i).getAwayTeamName());
+        }
+    }
+
+    @Test
+    void updateScoreShouldHandleMultipleMatchesSequentially() {
+        final int id_1 = scoreBoardLibrary.addNewGame("Mexico", "Canada" );
+        final int id_2 = scoreBoardLibrary.addNewGame("Spain", "Brazil" );
+        final int id_3 = scoreBoardLibrary.addNewGame("Germany", "France" );
+        final int id_4 = scoreBoardLibrary.addNewGame("Uruguay", "Italy" );
+        final int id_5 = scoreBoardLibrary.addNewGame("Argentina", "Australia" );
+
+        scoreBoardLibrary.updateScore(id_1, 2, 6);
+        scoreBoardLibrary.updateScore(id_2, 11, 3);
+        scoreBoardLibrary.updateScore(id_3, 3, 4);
+        scoreBoardLibrary.updateScore(id_4, 7, 6);
+        scoreBoardLibrary.updateScore(id_5, 4, 5);
+
+        scoreBoardLibrary.updateScore(id_1, 3, 7);
+        scoreBoardLibrary.updateScore(id_2, 14, 4);
+        scoreBoardLibrary.updateScore(id_3, 4, 5);
+        scoreBoardLibrary.updateScore(id_4, 8, 6);
+        scoreBoardLibrary.updateScore(id_5, 5, 5);
+
+        scoreBoardLibrary.updateScore(id_1, 5, 7);
+        scoreBoardLibrary.updateScore(id_2, 15, 7);
+        scoreBoardLibrary.updateScore(id_3, 5, 6);
+        scoreBoardLibrary.updateScore(id_4, 9, 6);
+        scoreBoardLibrary.updateScore(id_5, 6, 5);
+
+        var summary = scoreBoardLibrary.getASummary();
+
+        assertEquals(5, summary.size());
+
+        List<String[]> expectedTeams = List.of(
+                new String[]{"Spain", "Brazil"},
+                new String[]{"Uruguay", "Italy"},
+                new String[]{"Mexico", "Canada"},
+                new String[]{"Argentina", "Australia"},
+                new String[]{"Germany", "France"}
         );
 
         for (int i = 0; i < expectedTeams.size(); i++) {
@@ -146,12 +191,189 @@ public class LiveFootballWorldCupScoreBoardLibraryTests {
         scoreBoardLibrary.updateScore(id_4, 8, 6);
         scoreBoardLibrary.updateScore(id_5, 5, 5);
 
-
         scoreBoardLibrary.updateScore(id_1, 5, 7);
         scoreBoardLibrary.updateScore(id_2, 15, 7);
         scoreBoardLibrary.updateScore(id_3, 5, 6);
         scoreBoardLibrary.updateScore(id_4, 9, 6);
         scoreBoardLibrary.updateScore(id_5, 6, 5);
+
+        var summary = scoreBoardLibrary.getASummary();
+
+        assertEquals(5, summary.size());
+
+        List<String[]> expectedTeams = List.of(
+                new String[]{"Spain", "Brazil"},
+                new String[]{"Uruguay", "Italy"},
+                new String[]{"Mexico", "Canada"},
+                new String[]{"Argentina", "Australia"},
+                new String[]{"Germany", "France"}
+        );
+
+        for (int i = 0; i < expectedTeams.size(); i++) {
+            assertEquals(expectedTeams.get(i)[0], summary.get(i).getHomeTeamName());
+            assertEquals(expectedTeams.get(i)[1], summary.get(i).getAwayTeamName());
+        }
+    }
+//NOTE: non-deterministic behavior
+//    @Test
+//    void updateScoreShouldHandleMultipleMatchesSimultaneouslyViaParallelStream() throws InterruptedException {
+//        final int id_1 = scoreBoardLibrary.addNewGame("Mexico", "Canada" );
+//        final int id_2 = scoreBoardLibrary.addNewGame("Spain", "Brazil" );
+//        final int id_3 = scoreBoardLibrary.addNewGame("Germany", "France" );
+//        final int id_4 = scoreBoardLibrary.addNewGame("Uruguay", "Italy" );
+//        final int id_5 = scoreBoardLibrary.addNewGame("Argentina", "Australia" );
+//
+//// NOTE: non-deterministic behavior
+////        List<Runnable> tasks = List.of(
+////                () -> scoreBoardLibrary.updateScore(id_1, 2, 6),
+////                () -> scoreBoardLibrary.updateScore(id_2, 11, 3),
+////                () -> scoreBoardLibrary.updateScore(id_3, 3, 4),
+////                () -> scoreBoardLibrary.updateScore(id_4, 7, 6),
+////                () -> scoreBoardLibrary.updateScore(id_5, 4, 5),
+////
+////                () -> scoreBoardLibrary.updateScore(id_1, 3, 7),
+////                () -> scoreBoardLibrary.updateScore(id_2, 14, 4),
+////                () -> scoreBoardLibrary.updateScore(id_3, 4, 5),
+////                () -> scoreBoardLibrary.updateScore(id_4, 8, 6),
+////                () -> scoreBoardLibrary.updateScore(id_5, 5, 5),
+////
+////                () -> scoreBoardLibrary.updateScore(id_1, 5, 7),
+////                () -> scoreBoardLibrary.updateScore(id_2, 15, 7),
+////                () -> scoreBoardLibrary.updateScore(id_3, 5, 6),
+////                () -> scoreBoardLibrary.updateScore(id_4, 9, 6),
+////                () -> scoreBoardLibrary.updateScore(id_5, 6, 5)
+////        );
+//
+//        int numberOfTasks = 15; // Number of tasks
+//        CountDownLatch latch = new CountDownLatch(numberOfTasks);
+//
+//        List<Runnable> tasks = List.of(
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_1, 2, 6);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_2, 11, 3);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_3, 3, 4);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_4, 7, 6);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_5, 4, 5);
+//                    latch.countDown();
+//                },
+//
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_1, 3, 7);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_2, 14, 4);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_3, 4, 5);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_4, 8, 6);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_5, 5, 5);
+//                    latch.countDown();
+//                },
+//
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_1, 5, 7);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_2, 15, 7);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_3, 5, 6);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_4, 9, 6);
+//                    latch.countDown();
+//                },
+//                () -> {
+//                    scoreBoardLibrary.updateScore(id_5, 6, 5);
+//                    latch.countDown();
+//                }
+//        );
+//
+//        tasks.parallelStream().forEach(Runnable::run);
+//
+//        var summary = scoreBoardLibrary.getASummary();
+//
+//        assertEquals(5, summary.size());
+//
+//        List<String[]> expectedTeams = List.of(
+//                new String[]{"Spain", "Brazil"},
+//                new String[]{"Uruguay", "Italy"},
+//                new String[]{"Mexico", "Canada"},
+//                new String[]{"Argentina", "Australia"},
+//                new String[]{"Germany", "France"}
+//        );
+//
+//        for (int i = 0; i < expectedTeams.size(); i++) {
+//            assertEquals(expectedTeams.get(i)[0], summary.get(i).getHomeTeamName());
+//            assertEquals(expectedTeams.get(i)[1], summary.get(i).getAwayTeamName());
+//        }
+//    }
+
+    @Test
+    void updateScoreShouldHandleMultipleMatchesSimultaneouslyViaExecutorService() throws InterruptedException {
+        final int[] ids = new int[5];
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(5)) {
+
+            executor.submit(() -> ids[0] = scoreBoardLibrary.addNewGame("Mexico", "Canada" ));
+            executor.submit(() -> ids[1] = scoreBoardLibrary.addNewGame("Spain", "Brazil" ));
+            executor.submit(() -> ids[2] = scoreBoardLibrary.addNewGame("Germany", "France" ));
+            executor.submit(() -> ids[3] = scoreBoardLibrary.addNewGame("Uruguay", "Italy" ));
+            executor.submit(() -> ids[4] = scoreBoardLibrary.addNewGame("Argentina", "Australia" ));
+
+            executor.shutdown();
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        }
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(5)) {
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[0], 2, 6));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[1], 11, 3));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[2], 3, 4));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[3], 7, 6));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[4], 4, 5));
+
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[0], 3, 7));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[1], 14, 4));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[2], 4, 5));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[3], 8, 6));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[4], 5, 5));
+
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[0], 5, 7));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[1], 15, 7));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[2], 5, 6));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[3], 9, 6));
+            executor.submit(() -> scoreBoardLibrary.updateScore(ids[4], 6, 5));
+
+            executor.shutdown();
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        }
 
         var summary = scoreBoardLibrary.getASummary();
 
@@ -184,43 +406,29 @@ public class LiveFootballWorldCupScoreBoardLibraryTests {
 
     @Test
     void addNewGameShouldThrowExceptionForIdenticalTeamNames() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            final int id = scoreBoardLibrary.addNewGame("TeamA", "TeamA" );
-        });
+        assertThrows(IllegalArgumentException.class, () -> scoreBoardLibrary.addNewGame("TeamA", "TeamA" ));
     }
 
     @Test
     void addNewGameShouldThrowExceptionForNullTeamNames() {
-        assertThrows(NullPointerException.class, () -> {
-            final int id = scoreBoardLibrary.addNewGame(null, "TeamB" );
-        });
-        assertThrows(NullPointerException.class, () -> {
-            final int id = scoreBoardLibrary.addNewGame("TeamA", null);
-        });
+        assertThrows(NullPointerException.class, () -> scoreBoardLibrary.addNewGame(null, "TeamB" ));
+        assertThrows(NullPointerException.class, () -> scoreBoardLibrary.addNewGame("TeamA", null));
     }
 
     @Test
     void updateScoreShouldThrowExceptionForNegativeScores() {
         final int id = scoreBoardLibrary.addNewGame("TeamA", "TeamB" );
-        assertThrows(IllegalArgumentException.class, () -> {
-            scoreBoardLibrary.updateScore(id, -1, 3);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            scoreBoardLibrary.updateScore(id, 2, -3);
-        });
+        assertThrows(IllegalArgumentException.class, () -> scoreBoardLibrary.updateScore(id, -1, 3));
+        assertThrows(IllegalArgumentException.class, () -> scoreBoardLibrary.updateScore(id, 2, -3));
     }
 
     @Test
     void updateScoreShouldThrowExceptionForNonExistentGame() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            scoreBoardLibrary.updateScore(1234567890, 1, 2);
-        });
+        assertThrows(IllegalArgumentException.class, () -> scoreBoardLibrary.updateScore(1234567890, 1, 2));
     }
 
     @Test
     void finishGameShouldThrowExceptionForNonExistentGame() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            scoreBoardLibrary.finishGame(1234567890 );
-        });
+        assertThrows(IllegalArgumentException.class, () -> scoreBoardLibrary.finishGame(1234567890));
     }
 }
